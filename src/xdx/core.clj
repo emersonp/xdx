0aba3e0c10cb9263b376466269876bd280f485ab
emersonp <parker.emerson@gmail.com>
2015-01-21 19:27:43 -0800

    Cleaned Up roll-dice Function
    Switched out recursive call for repeatedly on roll-dice. Also removed unrelated get-input function.

--- src/xdx/core.clj
+++ src/xdx/core.clj
@@ -1,25 +1,10 @@
 (ns xdx.core
   (:gen-class))
 
-(defn get-input
-  "Waits for user to enter a number of dice and hit enter, then cleans the input."
-  ([] (get-input "Please enter input. [DEFAULT MESSAGE]" nil))
-  ([default] (get-input "Please enter input. [DEFAULT MESSAGE]" default))
-  ([message default]
-   (prn message)
-   (let [input (clojure.string/trim (read-line))]
-     (if (empty? input)
-       default
-       (clojure.string/lower-case input)))))
-
 (defn roll-dice
   "Rolls a 'dice' number of dice with 'sides' sides."
   [dice sides]
-  (loop [left-to-roll dice
-         total 0]
-    (if (zero? left-to-roll)
-      total
-      (recur (dec left-to-roll) (+ total (inc (rand-int sides)))))))
+  (reduce + (repeatedly dice #(inc (rand-int sides)))))
 
 (defn roll-explode
   "Rolls a 'dice' number of dice with 'sides' sides, with the dice _exploding_ if a max val is rolled."
